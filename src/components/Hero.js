0cc13fb6a70f655481720e717d99bdda47055d60
vijethx <38877972+vijethx@users.noreply.github.com>
2022-02-01 11:42:40 +0530

    Added alt text to images

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,18 @@
-import React from "react"
+import React from "react";
 
 export default function Hero() {
-    return (
-        <section className="hero">
-            <img src="../images/photo-grid.png" className="hero--photo" />
-            <h1 className="hero--header">Online Experiences</h1>
-            <p className="hero--text">Join unique interactive activities led by 
-            one-of-a-kind hosts—all without leaving home.</p>
-        </section>
-    )
+	return (
+		<section className="hero">
+			<img
+				src="../images/photo-grid.png"
+				className="hero--photo"
+				alt="hero"
+			/>
+			<h1 className="hero--header">Online Experiences</h1>
+			<p className="hero--text">
+				Join unique interactive activities led by one-of-a-kind
+				hosts—all without leaving home.
+			</p>
+		</section>
+	);
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React from "react";
 
 export default function Navbar() {
-    return (
-        <nav>
-            <img src="../images/airbnb-logo.png" className="nav--logo" />
-        </nav>
-    )
+	return (
+		<nav>
+			<img
+				src="../images/airbnb-logo.png"
+				className="nav--logo"
+				alt="logo"
+			/>
+		</nav>
+	);
 }
